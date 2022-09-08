11e9425116fad98d162f4ecc1a01caab1d2b46f0
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-09-07 22:59:14 -0300

    feat: POST /signin

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
 import * as authTypes from "../types/authTypes";
 import * as authRepository from "../repositories/authRepository";
 import * as authUtils from "../utils/authUtils";
+import dotenv from "dotenv";
 
 dotenv.config();
 
